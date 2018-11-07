266d8d51b0b3ef179d02401b5cce1662c6042e82
Huangxiaoyuyuyu <253757635@qq.com>
2018-11-07 16:08:06 +0800

    pinyougou-manager-web

---
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -0,0 +1,70 @@
+package com.pinyougou.manager.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
+import entity.PageResult;
+import entity.Result;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/brand")
+public class BrandController {
+
+    @Reference
+    private BrandService brandService;
+
+    @RequestMapping("/findAll")
+    public List<TbBrand> findAll() {
+        return brandService.findAll();
+    }
+
+    @RequestMapping("/findPage")
+    public PageResult findPage(int pageNum, int pageSize) {
+        return brandService.findpage(pageNum,pageSize);
+    }
+
+    @RequestMapping("/add")
+    public Result add(@RequestBody TbBrand tbBrand) {
+        try {
+            brandService.add(tbBrand);
+            return new Result(true,"追加成功~");
+        }catch (Exception e) {
+            return new Result(false,"追加失败~");
+        }
+    }
+
+    @RequestMapping("/findOne")
+    public TbBrand findOne(Long id) {
+         return brandService.findOne(id);
+    }
+
+    @RequestMapping("/update")
+    public Result update(@RequestBody TbBrand tbBrand) {
+        try {
+            brandService.update(tbBrand);
+            return new Result(true,"更新成功~");
+        }catch (Exception e) {
+            return new Result(false,"更新失败~");
+        }
+    }
+
+    @RequestMapping("/delete")
+    public Result delete(Long[] ids) {
+        try {
+            brandService.delete(ids);
+            return new Result(true,"删除成功~");
+        }catch (Exception e) {
+            return new Result(false,"删除失败~");
+        }
+    }
+
+    @RequestMapping("/search")
+    public PageResult search(@RequestBody TbBrand tbBrand,int pageNum,int pageSize) {
+        return brandService.search(tbBrand,pageNum,pageSize);
+    }
+}
