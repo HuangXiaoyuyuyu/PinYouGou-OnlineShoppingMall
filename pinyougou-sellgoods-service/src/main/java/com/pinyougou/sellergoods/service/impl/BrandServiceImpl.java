0d509795db9e24b6ca3740088218f42665c4fb98
Huangxiaoyuyuyu <253757635@qq.com>
2018-11-07 16:10:00 +0800

    pinyougou-sellgoods-service

---
+++ pinyougou-sellgoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,74 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.mapper.TbBrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.pojo.TbBrandExample;
+import com.pinyougou.sellergoods.service.BrandService;
+import entity.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+public class BrandServiceImpl implements BrandService {
+
+    @Autowired
+    private TbBrandMapper brandMapper;
+
+    @Override
+    public List<TbBrand> findAll() {
+        return brandMapper.selectByExample(null) ;
+    }
+
+    @Override
+    public PageResult findpage(int pageNum, int pageSize) {
+
+        PageHelper.startPage(pageNum,pageSize);
+        Page<TbBrand> result = (Page<TbBrand>) brandMapper.selectByExample(null);
+        return new PageResult(result.getTotal(),result.getResult());
+    }
+
+    @Override
+    public void add(TbBrand tbBrand) {
+        brandMapper.insert(tbBrand);
+    }
+
+    @Override
+    public TbBrand findOne(Long id) {
+        return brandMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(TbBrand tbBrand) {
+        brandMapper.updateByPrimaryKey(tbBrand);
+    }
+
+    @Override
+    public void delete(Long[] ids) {
+        for (Long id : ids) {
+            brandMapper.deleteByPrimaryKey(id);
+        }
+    }
+
+    @Override
+    public PageResult search(TbBrand tbBrand, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+
+        TbBrandExample example = new TbBrandExample();
+        TbBrandExample.Criteria criteria = example.createCriteria();
+        if (tbBrand != null) {
+            if (tbBrand.getName() != null && tbBrand.getName().length() > 0) {
+                criteria.andNameLike("%"+tbBrand.getName()+"%");
+            }
+            if (tbBrand.getFirstChar() != null && tbBrand.getFirstChar().length() > 0) {
+                criteria.andFirstCharLike("%"+tbBrand.getFirstChar()+"%");
+            }
+        }
+        Page<TbBrand> brands = (Page<TbBrand>) brandMapper.selectByExample(example);
+        return new PageResult(brands.getTotal(),brands.getResult());
+    }
+}
