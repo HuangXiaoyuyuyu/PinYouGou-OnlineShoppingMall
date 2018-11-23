0142bbfc523d23e756fa96497947c0a79cbf5727
Huangxiaoyuyuyu <253757635@qq.com>
2018-11-23 15:10:14 +0800

    pinyougou-content-manager-content

--- pinyougou-manager-web/src/main/webapp/js/controller/contentController.js
+++ pinyougou-manager-web/src/main/webapp/js/controller/contentController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('contentController' ,function($scope,$controller   ,contentService){	
+app.controller('contentController' ,function($scope,$controller,uploadService,contentService,contentCategoryService){
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
@@ -10,7 +10,7 @@ app.controller('contentController' ,function($scope,$controller   ,contentServic
 				$scope.list=response;
 			}			
 		);
-	}    
+	};
 	
 	//分页
 	$scope.findPage=function(page,rows){			
@@ -20,7 +20,7 @@ app.controller('contentController' ,function($scope,$controller   ,contentServic
 				$scope.paginationConf.totalItems=response.total;//更新总记录数
 			}			
 		);
-	}
+	};
 	
 	//查询实体 
 	$scope.findOne=function(id){				
@@ -29,7 +29,7 @@ app.controller('contentController' ,function($scope,$controller   ,contentServic
 				$scope.entity= response;					
 			}
 		);				
-	}
+	};
 	
 	//保存 
 	$scope.save=function(){				
@@ -49,7 +49,7 @@ app.controller('contentController' ,function($scope,$controller   ,contentServic
 				}
 			}		
 		);				
-	}
+	};
 	
 	 
 	//批量删除 
@@ -63,7 +63,7 @@ app.controller('contentController' ,function($scope,$controller   ,contentServic
 				}						
 			}		
 		);				
-	}
+	};
 	
 	$scope.searchEntity={};//定义搜索对象 
 	
@@ -75,6 +75,31 @@ app.controller('contentController' ,function($scope,$controller   ,contentServic
 				$scope.paginationConf.totalItems=response.total;//更新总记录数
 			}			
 		);
-	}
+	};
+
+    //图片上传
+    $scope.uploadFile = function () {
+        uploadService.uploadFile().success(
+            function (response) {
+                if (response.success) {
+                    $scope.entity.pic = response.message;
+                } else {
+                    alert(response.message);
+                }
+            }).error(
+            	function () {
+					alert("上传出错~");
+                }
+		)
+    };
+
+    //查询所有contentCategory
+    $scope.findContentCategoryList = function () {
+		contentCategoryService.findAll().success(
+			function (response) {
+				$scope.contentCategoryList = response;
+            }
+		)
+    }
     
 });	
