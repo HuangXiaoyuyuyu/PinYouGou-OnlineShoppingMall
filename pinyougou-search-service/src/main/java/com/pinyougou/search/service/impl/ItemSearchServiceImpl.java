5c69b5727944720417421640136e7353a30b749a
Huangxiaoyuyuyu <253757635@qq.com>
2018-11-28 11:07:32 +0800

    pinyougou-search-interface-interface-impl

---
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -0,0 +1,37 @@
+package com.pinyougou.search.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.pojo.TbItem;
+import com.pinyougou.search.service.ItemSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.query.Criteria;
+import org.springframework.data.solr.core.query.Query;
+import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.result.ScoredPage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author OliverYu
+ * @Date 2018/11/28 10:25
+ * @Description TODO
+ */
+@Service
+public class ItemSearchServiceImpl implements ItemSearchService {
+
+    @Autowired
+    SolrTemplate solrTemplate;
+
+    @Override
+    public Map search(Map searchMap) {
+        Map map = new HashMap();
+        Query query = new SimpleQuery("*:*");
+        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+        query.addCriteria(criteria);
+        ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
+        map.put("rows",page.getContent());
+        return map;
+    }
+}
