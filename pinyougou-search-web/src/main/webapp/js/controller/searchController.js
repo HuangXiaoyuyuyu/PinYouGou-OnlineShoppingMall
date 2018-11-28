908c55d6ca79da6017bb38c783a39b0d7df3e2e2
Huangxiaoyuyuyu <253757635@qq.com>
2018-11-28 13:15:48 +0800

    pinyougou-search-web-keywords-search-FE

---
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -0,0 +1,9 @@
+app.controller("searchController",function ($scope,searchService) {
+    $scope.search = function () {
+        searchService.search($scope.searchMap).success(
+            function (response) {
+                $scope.resultMap = response;
+            }
+        )
+    }
+});
