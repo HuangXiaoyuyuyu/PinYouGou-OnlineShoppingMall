8aee042a798b188fa7234556178e3e377171576e
Huangxiaoyuyuyu <253757635@qq.com>
2018-11-28 10:24:51 +0800

    pinyougou-search-interface-interface

---
+++ pinyougou-search-interface/src/main/java/com/pinyougou/search/service/ItemSearchService.java
@@ -0,0 +1,18 @@
+package com.pinyougou.search.service;
+
+import java.util.Map;
+
+/**
+ * @Author OliverYu
+ * @Date 2018/11/28 10:20
+ * @Description TODO
+ */
+public interface ItemSearchService {
+    /**
+     * 搜索方法
+     * @param searchMap
+     * @return
+     */
+    public Map search(Map searchMap);
+
+}
