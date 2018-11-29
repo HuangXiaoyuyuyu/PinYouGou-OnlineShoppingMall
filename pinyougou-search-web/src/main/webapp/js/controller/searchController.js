13ae5b17cab2a13209ad3816429195cb19a3f478
Huangxiaoyuyuyu <253757635@qq.com>
2018-11-29 15:52:40 +0800

    pinyougou-manager-web-construct-filter-conditions

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,9 +1,33 @@
 app.controller("searchController",function ($scope,searchService) {
+
+    //搜索对象
+    $scope.searchMap = {'keywords':'','category':'','brand':'','spec':{}};
+
     $scope.search = function () {
         searchService.search($scope.searchMap).success(
             function (response) {
                 $scope.resultMap = response;
             }
         )
+    };
+
+    //添加搜索项
+    $scope.addSearchItem = function (key,value) {
+        if (key == 'category' || key == 'brand') {
+            $scope.searchMap[key] = value;
+        }else {
+            $scope.searchMap.spec[key] = value;
+        }
+        $scope.search();//执行搜索
+    };
+
+    //移除搜索项
+    $scope.removeSearchItem = function (key) {
+        if (key == 'category' || key == 'brand') {
+            $scope.searchMap[key] = '';
+        }else {
+            delete $scope.searchMap.spec[key];//移除此属性
+        }
+        $scope.search();//执行搜索
     }
 });
