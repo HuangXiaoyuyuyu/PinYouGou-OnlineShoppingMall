eb782600edc24964754daa577cdb94defa0e6a14
Huangxiaoyuyuyu <253757635@qq.com>
2018-12-03 10:51:25 +0800

    pinyougou-manager-web-price range-FE

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,7 +1,7 @@
 app.controller("searchController",function ($scope,searchService) {
 
     //搜索对象
-    $scope.searchMap = {'keywords':'','category':'','brand':'','spec':{}};
+    $scope.searchMap = {'keywords':'','category':'','brand':'','spec':{},'price':''};
 
     $scope.search = function () {
         searchService.search($scope.searchMap).success(
@@ -13,7 +13,7 @@ app.controller("searchController",function ($scope,searchService) {
 
     //添加搜索项
     $scope.addSearchItem = function (key,value) {
-        if (key == 'category' || key == 'brand') {
+        if (key == 'category' || key == 'brand' || key == 'price') {
             $scope.searchMap[key] = value;
         }else {
             $scope.searchMap.spec[key] = value;
@@ -23,7 +23,7 @@ app.controller("searchController",function ($scope,searchService) {
 
     //移除搜索项
     $scope.removeSearchItem = function (key) {
-        if (key == 'category' || key == 'brand') {
+        if (key == 'category' || key == 'brand' || key == 'price') {
             $scope.searchMap[key] = '';
         }else {
             delete $scope.searchMap.spec[key];//移除此属性
