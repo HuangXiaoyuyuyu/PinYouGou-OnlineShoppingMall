da35c2ab94507ed0c9e0f4a0a60c502fdee7718b
Huangxiaoyuyuyu <253757635@qq.com>
2019-01-02 16:07:52 +0800

    pinyougou-task-service-seckillGoods-increment-update-redis

--- pinyougou-task-service/src/main/java/com/pinyougou/task/SeckillTask.java
+++ pinyougou-task-service/src/main/java/com/pinyougou/task/SeckillTask.java
@@ -1,9 +1,16 @@
 package com.pinyougou.task;
 
+import com.pinyougou.mapper.TbSeckillGoodsMapper;
+import com.pinyougou.pojo.TbSeckillGoods;
+import com.pinyougou.pojo.TbSeckillGoodsExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @Author OliverYu
@@ -12,12 +19,40 @@ import java.util.Date;
  */
 @Component
 public class SeckillTask {
+
+    @Autowired
+    private TbSeckillGoodsMapper seckillGoodsMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 刷新秒杀商品
      */
     @Scheduled(cron = "* * * * * ?")
     public void refreshSeckillGoods() {
-        System.out.println("执行了任务调度：" + new Date());
+
+        System.out.println("执行了秒杀商品增量更新 任务调度 " + new Date());
+
+        List goodsIdList = new ArrayList(redisTemplate.boundHashOps("seckillGoods").keys());
+
+        TbSeckillGoodsExample example = new TbSeckillGoodsExample();
+        TbSeckillGoodsExample.Criteria criteria = example.createCriteria();
+        criteria.andStatusEqualTo("1");//审核通过
+        criteria.andStockCountGreaterThan(0);//剩余库存大于0
+        criteria.andStartTimeLessThanOrEqualTo(new Date());//开始时间小于等于当前时间
+        criteria.andEndTimeGreaterThan(new Date());//结束时间大于当前时间
+
+        if (goodsIdList.size() > 0) {
+            criteria.andIdNotIn(goodsIdList);
+        }
+
+        List<TbSeckillGoods> seckillGoodsList = seckillGoodsMapper.selectByExample(example);
+        //将商品放入缓存
+        System.out.println("将商品放入缓存");
+        for (TbSeckillGoods seckillGoods : seckillGoodsList) {
+            redisTemplate.boundHashOps("seckillGoods").put(seckillGoods.getId(),seckillGoods);
+            System.out.println("增量更新秒杀商品ID：" + seckillGoods.getId());
+        }
     }
 
 }
